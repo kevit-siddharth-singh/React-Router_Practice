ad19e3360cd0cb227af03f2078bac4be43de5e05
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-07-30 11:48:47 +0530

    Repo Added

---
+++ frontend/src/Pages/NewEventPagejsxNewEventPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewEventPagejsxNewEventPage = () => {
+  return (
+    <div>NewEventPagejsxNewEventPage</div>
+  )
+}
+
+export default NewEventPagejsxNewEventPage
